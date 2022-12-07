ee03eb3b7b8d0881762c84b3911bfe98e02584b6
ghassenayari <ghassen.ayari@gmail.com>
2022-12-07 21:02:18 +0100

    extend converter factory test coverage.

--- src/test/java/com/supermarket/pricer/weight/ConverterFactoryTest.java
+++ src/test/java/com/supermarket/pricer/weight/ConverterFactoryTest.java
@@ -3,8 +3,7 @@ package com.supermarket.pricer.weight;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static com.supermarket.pricer.weight.WeightUnity.GRAM;
-import static com.supermarket.pricer.weight.WeightUnity.OUNCE;
+import static com.supermarket.pricer.weight.WeightUnity.*;
 
 class ConverterFactoryTest {
 
@@ -20,5 +19,33 @@ class ConverterFactoryTest {
         WeightConverter converter = ConverterFactory.getConverter(GRAM, OUNCE).get();
         float result = converter.convert(new Weight(0.17636684f, OUNCE));
         Assertions.assertEquals(5f, result);
+    }
+
+    @Test
+    void convert_Positive_FromGramToPound() {
+        WeightConverter converter = ConverterFactory.getConverter(GRAM, POUND).get();
+        float result = converter.convert(new Weight(100f, GRAM));
+        Assertions.assertEquals(0.22045855f, result);
+    }
+
+    @Test
+    void convert_Positive_FromPoundToGram() {
+        WeightConverter converter = ConverterFactory.getConverter(GRAM, POUND).get();
+        float result = converter.convert(new Weight(00.22045855f, POUND));
+        Assertions.assertEquals(100f, result);
+    }
+
+    @Test
+    void convert_Positive_FromOunceToPound() {
+        WeightConverter converter = ConverterFactory.getConverter(OUNCE, POUND).get();
+        float result = converter.convert(new Weight(6.25f, OUNCE));
+        Assertions.assertEquals(0.390625, result);
+    }
+
+    @Test
+    void convert_Positive_FromPoundToOunce() {
+        WeightConverter converter = ConverterFactory.getConverter(OUNCE, POUND).get();
+        float result = converter.convert(new Weight(2400f, POUND));
+        Assertions.assertEquals(38400f, result);
     }
 }
